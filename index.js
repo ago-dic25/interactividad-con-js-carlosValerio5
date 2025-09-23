a7b7332f386174d0654c60764721a39a5831955f
Carlos Valerio <carlosvalerio@Carloss-MacBook-Air.local>
2025-09-22 21:36:28 -0600

    Adds features discussed in README

--- index.js
+++ index.js
@@ -1,41 +1,139 @@
-//====Variables=====//
-var arreglo = [1, 2, 3, 4, 5];
-var entero = 1;
-var cadena_texto = "Hola Mundo";
-var booleano = true;
+const x = "Hello World";
 
-//===Constantes====//
-const PI = 3.1416;
-
-
-//====Funciones====//
-function suma(x, y){
-    var resultado = x + y;
-    console.log("El resultado de la suma es: " + resultado);
-    return resultado;
+function sum(x, y){
+    return x+y;
 }
 
-function imprimirMensaje(mensaje){
-    console.log("Este es mi mensaje " + mensaje);
+const imprimirMensaje = (mensaje) => {
+    console.log(mensaje);
 }
 
-imprimirMensaje("Hola");
-imprimirMensaje("Vacaciones 1 semana :D");
-imprimirMensaje(true);
 
-suma(1, 2);
-suma(10, 20);
-suma(100, 200);
+// Elementos de la pagina
+const botonBuscar = document.getElementById("button-search");
 
 
-//===Elementos de la pagina como variables===///
-var botonBuscar = document.getElementById("buscar");
-
-console.log(botonBuscar);
-
-//==== Eventos===== //
+// Eventos
 botonBuscar.addEventListener("click", function(){
-    //alert("Hiciste clic al boton");
+    //alert("Hiciste click al boton.");
     botonBuscar.classList.add("rojo");
+})
+
+const linkPosts = document.getElementById("see-posts")
+const listPosts =  document.getElementById("list-posts")
+
+
+if (linkPosts!=null){
+    linkPosts.addEventListener("click", function(){
+        listPosts.classList.remove("ocultar");
+    })
+}
+
+var mas = document.getElementById("mas");
+var verTodos = document.getElementById("ver-todos");
+var posts = document.getElementById("posts");
+var nightMode = document.getElementById("night-mode");
+var body = document.body;
+
+mas.addEventListener('click', function(){
+    alert("Seccion en progreso");
+})
+
+verTodos.addEventListener('click', function(){
+    if (posts.classList.contains("ocultar"))
+        posts.classList.replace("ocultar", "mostrar");
+    else
+        posts.classList.replace("mostrar", "ocultar");
+})
+
+
+nightMode.addEventListener('click', function(){
+	if (body.classList.contains('dark-mode')){
+		body.classList.replace('dark-mode', 'light-mode');
+	}	
+	else
+		body.classList.replace('light-mode', 'dark-mode');
+})
+
+// likes
+let countPost1 = 0;
+let countPost2 = 0;
+let countPost3 = 0;
+
+const post1 = document.getElementById('post-1');
+const post2 = document.getElementById('post-2');
+const post3 = document.getElementById('post-3');
+
+const counter1 = document.getElementById('counter-1');
+const counter2 = document.getElementById('counter-2');
+const counter3 = document.getElementById('counter-3');
+
+function updateDisplay(counter, count){
+	counter.textContent = count;
+}
+
+post1.addEventListener('click', ()=>{
+	countPost1++;
+	updateDisplay(counter1, countPost1);
 });
 
+post2.addEventListener('click', ()=>{
+	countPost2++;
+	updateDisplay(counter2, countPost2);
+});
+post3.addEventListener('click', ()=>{
+	countPost3++;
+	updateDisplay(counter3, countPost3);
+});
+
+//Search field
+
+const inputField = document.getElementById('search-field');
+const tarjetas = document.getElementsByClassName('post');
+const searchButton = document.getElementById('button-search');
+
+searchButton.addEventListener('click', ()=>{
+	let inputText = inputField.value.toLowerCase();
+
+	Array.from(tarjetas).forEach(div => {
+		const divContent = div.textContent.toLowerCase();
+		if (divContent.includes(inputText))
+			div.classList.add('mostrar');
+		else
+			div.classList.add('ocultar');
+	});
+});
+
+
+// Gallery counter
+function sleep(ms){
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+var verFotos = document.getElementById("ver-fotos");
+const galleryItems = document.getElementsByClassName('grid-item');
+const imageGallery = document.getElementById('image-gallery');
+const pics = document.getElementById('pics'); 
+verFotos.addEventListener('click', () => {
+	pics.classList.toggle('ocultar');
+});
+
+let isGalleryRunning = false;
+
+pics.addEventListener('mouseenter', async function(event){
+	isGalleryRunning = true;
+	while (isGalleryRunning){
+		for (const image of Array.from(galleryItems)) {
+			if (image.classList.contains('ocultar')) {
+				image.classList.replace('ocultar', 'mostrar');
+			}
+			await sleep(3000);
+			if (!isGalleryRunning) break;
+			image.classList.replace('mostrar', 'ocultar');
+
+		}
+	}
+});
+
+pics.addEventListener('mouseleave', async function(event){
+	isGalleryRunning = false;
+});
